926f0294c6f8ca4d121e87fe9737826c3da848df
dstineback <dstineback@gmail.com>
2016-03-17 11:49:42 -0700

    array update

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-/**
+var rightAnswers = [];
+
 var userName = prompt('Time to take a quiz about ME! First off what is your name?');
 alert('Nice to meet you ' + userName + ' time to start the quiz.');
 console.log('get user name');
@@ -7,6 +8,7 @@ var whereBorn = prompt('Do you think I was born in Washington?');
 if ((whereBorn.toLowerCase() === 'yes') || (whereBorn.toLowerCase() === 'y')) {
   console.log('Yes to born');
   alert('You are correct!');
+  rightAnswers.push(true);
 } else if ((whereBorn.toLowerCase() === ('no')) || (whereBorn.toLowerCase() === ('n'))) {
   console.log('No to born');
   alert('Believe it or not I have never lived outside of Washington');
@@ -19,6 +21,7 @@ var married = prompt('Do you think I am married?');
 if ((married.toLowerCase() === 'yes') || (married.toLowerCase() === 'y')) {
   console.log('Yes to being married');
   alert('I am married to my beautiful wife Ashley.');
+  rightAnswers.push(true);
 } else if ((married.toLowerCase() === ('no')) || (married.toLowerCase() === ('n'))) {
   console.log('no about being married');
   alert('Wrong, I have been married since 2009');
@@ -31,6 +34,7 @@ var kids = prompt('Do you think I have any kids?');
 if ((kids.toLowerCase() === 'yes') || (kids.toLowerCase() === 'y')) {
   console.log('Yes to kids');
   alert('I do! I have a three year old boy named Henry.');
+  rightAnswers.push(true);
 } else if ((kids.toLowerCase() === ('no')) || (kids.toLowerCase() === ('n'))) {
   console.log('No to kids');
   alert('I do have a kid even though you do not think I should.');
@@ -43,6 +47,7 @@ var pets = prompt('Do you think I own any pets?');
 if ((pets.toLowerCase() === 'yes') || (pets.toLowerCase() === 'y')) {
   console.log('Yes to pets');
   alert('Absolutely not, kids are enough!');
+  rightAnswers.push(true);
 } else if ((pets.toLowerCase() === ('no')) || (pets.toLowerCase() === ('n'))) {
   console.log('No to pets');
   alert('You are right. I do not have time for pets');
@@ -55,9 +60,11 @@ var userAnswer = prompt('Do you like soccer?');
 if ((userAnswer.toLowerCase() === 'yes') || (userAnswer.toLowerCase() === 'y')) {
   console.log('user does like soccer');
   var soccer = prompt('You do! Do you watch the Sounders?');
+  rightAnswers.push(true);
   if ((soccer.toLowerCase() === 'yes') || (soccer.toLowerCase() === 'y')){
     console.log('yes to Sounders');
     alert('Cool, I watch every game!');
+    rightAnswers.push(true);
   } else if ((soccer.toLowerCase() === 'no') || (soccer.toLowerCase() === 'n')) {
     console.log('no on watching the Sounders');
     alert('Bummer, you should give them a try');
@@ -69,26 +76,24 @@ if ((userAnswer.toLowerCase() === 'yes') || (userAnswer.toLowerCase() === 'y')) 
   console.log('Did not understand your answer for soccer');
   alert('Did not understand your answer. Make sure to answer yes or no');
 }
-**/
 
-
-
-var sonAge = 3;
-
-
-for(var i = 0;( i < 4 || userGuess === sonAge); i++) {
-var userGuess = prompt('What is the age of my son?');
-    console.log('loop Q1');
-    if (userGuess > sonAge) {
+for(var i = 0;( i < 4 && userGuess === sonAge); i++) {
+  var sonAge = 3;
+  var userGuess = prompt('What is the age of my son?');
+  console.log('loop Q1');
+  if (userGuess > sonAge) {
     console.log('Guess too high');
     alert('Nope, too high');
   } else if (userGuess < sonAge) {
     console.log('Guess too low');
     alert('Nope, too low');
-  } else if (isNan(userGuess)) {
+  } else if (isNaN(userGuess)) {
     alert('not a number');
-  }else {
+  } else {
     console.log('Guess is correct');
     alert('You are correct!');
+    rightAnswers.push(true);
   }
 }
+
+alert('Good job you got ' + rightAnswers.length + ' out of 7 questions right!');
